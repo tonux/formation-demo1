3e0f1be49e734322339b34012df3d2460a0ad30d
tonux <sambndongo@gmail.com>
2022-05-25 15:48:56 -0400

    ajouter les tests, repository

--- src/test/java/com/ca/formation/formationdemo1/controllers/PersonneControllerTest.java
+++ src/test/java/com/ca/formation/formationdemo1/controllers/PersonneControllerTest.java
@@ -1,5 +1,7 @@
 package com.ca.formation.formationdemo1.controllers;
 
+import com.ca.formation.formationdemo1.FormationDemo1Application;
+import com.ca.formation.formationdemo1.models.Personne;
 import com.ca.formation.formationdemo1.services.PersonneService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,8 +9,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.*;
 import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
@@ -16,11 +19,10 @@ import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.*;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc
 public class PersonneControllerTest {
 
@@ -29,6 +31,21 @@ public class PersonneControllerTest {
 
     @MockBean
     PersonneService personneService;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @LocalServerPort
+    private int port;
+
+    private String getRootUrl(){
+        return "http://localhost:" + port + "/api/v2";
+    }
+
+    @Test
+    public void helloTest(){
+        assertEquals(this.restTemplate.getForObject("http://localhost:" + port + "/api/v2/personnes/hello", String.class), "Bonjour tout le monde");
+    }
 
     @Test
     public void getPersonnes() throws Exception{
@@ -41,6 +58,58 @@ public class PersonneControllerTest {
         MockHttpServletResponse response = mvcResult.getResponse();
 
         assertEquals(HttpStatus.OK.value(), response.getStatus());
+    }
+
+
+    @Test
+    public void getPersonnesVrai() throws Exception{
+        HttpHeaders headers = new HttpHeaders();
+        HttpEntity<String> entity = new HttpEntity<String>(null, headers);
+
+        ResponseEntity<String> response = restTemplate.exchange("http://localhost:" + port + "/api/v2/personnes",
+                HttpMethod.GET, entity, String.class);
+
+        assertNotNull(response.getBody());
+
+    }
+
+    @Test
+    public void getPersonne_() throws Exception{
+        HttpHeaders headers = new HttpHeaders();
+        HttpEntity<String> entity = new HttpEntity<String>(null, headers);
+
+        Personne personne = restTemplate.getForObject(getRootUrl()+"/personnes/3", Personne.class);
+
+        assertNotNull(personne);
+        assertEquals(personne.getNom(), "Abdel");
+
+    }
+
+    @Test
+    public void ajouterPersonne_() throws Exception{
+        HttpHeaders headers = new HttpHeaders();
+        HttpEntity<String> entity = new HttpEntity<String>(null, headers);
+
+        Personne personne = restTemplate.postForObject(getRootUrl()+"/personnes", new Personne("tonux", "samb", 40), Personne.class);
+
+        assertNotNull(personne);
+        assertEquals(personne.getNom(), "tonux");
+
+    }
+
+    @Test
+    public void getPersonne() throws Exception{
+        RequestBuilder requestBuilder = MockMvcRequestBuilders
+                .get("/api/v2/personnes/2")
+                .contentType(MediaType.APPLICATION_JSON);
+
+        MvcResult mvcResult = mockMvc.perform(requestBuilder).andReturn();
+        String contentAsString = mvcResult.getResponse().getContentAsString();
+
+        System.out.println(contentAsString);
+        assertNotNull(contentAsString);
+
+
     }
 
 }
